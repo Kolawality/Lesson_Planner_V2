73540e14766f7d7cb1d54c4708d93abd157512d9
Michael Kolawole <Michael.Kolawole@gmail.com>
2021-03-18 20:34:07 +0000

    you can add success criteria to plan (no format)

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ window.addEventListener('load', () => {
 
     const params = (new URL(document.location)).searchParams;
     const objective = params.get('objective');
+    const success1 = params.get('success1');
 
     document.getElementById('objective-entered').innerHTML = objective;
+    document.getElementById('success1').innerHTML = success1;
 })
 
 
